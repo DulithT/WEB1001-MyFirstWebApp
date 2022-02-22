12fc25c0ba34c138a92cf6aebdee2563ea59f25d
DulithT <duliththaveesha@gmail.com>
2022-02-22 02:40:23 -0500

    Added forms

--- WEB1001-MyFirstWebApp-CS/Pages/Intro.cshtml.cs
+++ WEB1001-MyFirstWebApp-CS/Pages/Intro.cshtml.cs
@@ -5,6 +5,11 @@ namespace WEB1001_MyFirstWebApp_CS.Pages
 {
     public class IntroModel : PageModel
     {
+
+        public int Int1 { get; set; } = 0;
+        public int Int2 { get; set; } = 0;
+
+
         public void OnGet()
         {
         }
